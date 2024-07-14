16202dada513aa26538fcc947aa29bd5217c372e
Krisha Kalola <121624415+KrishaKalola@users.noreply.github.com>
2024-07-15 01:19:57 +0530

    Add files via upload

---
+++ Day1/Q283.java
@@ -0,0 +1,20 @@
+// 283. Moves zeros
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int zeroes=0;
+        int j=0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                zeroes++;
+            }
+            else{
+                nums[j++]=nums[i];
+            }
+        }
+        for(int i=0;i<zeroes;i++){
+            nums[j++]=0;
+        }
+    }
+}
